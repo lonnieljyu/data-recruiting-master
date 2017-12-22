bd7fd7b04b67a4c0e8d7cafca935c93c02575f7e
Lonnie Yu <lonnieljyu@gmail.com>
2017-12-22 04:22:35 -0500

    Implement symbol_aggregates GET logic

--- app/server/elasticsearch_interface.py
+++ app/server/elasticsearch_interface.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 from data_interface import DataInterface
 from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
 from elasticsearch_dsl.connections import connections
 
 
@@ -65,8 +66,56 @@ class ElasticSearchInterface(DataInterface):
         }
         self.es.update(index=self.index, doc_type=self.doc_type, id=uuid, body=script)
 
+    def get_date_stats(self, symbol):
+        '''
+        Queries and returns the date stats for the given symbol.
+
+        :param symbol: Symbol in the alphabet
+        :type symbol: string
+        :return result: Stats of the symbol
+        :rtype result: dict
+        '''
+        response = Search(using=self.es, index=self.index)\
+            .query('match', symbol=symbol)\
+            .aggs.metric('date_stats', 'stats', field='date')\
+            .execute()
+        stats = response.aggregations.date_stats
+        result = {
+            'symbol': symbol,
+            'count': stats.count,
+            'earliest': stats.min_as_string,
+            'latest': stats.max_as_string
+        }
+        return result
+
+    def get_followups_count(self, symbol):
+        '''
+        Queries and counts the number of followups for the given symbol.
+
+        :param symbol: Symbol in the alphabet
+        :type symbol: string
+        :return count: Followups count
+        :rtype count: int
+        '''
+        count = 0
+        query = Search(using=self.es, index=self.index) \
+            .query('match', symbol=symbol)
+        for hit in query.scan():
+            count += len(hit.followups)
+        return count
+
     def symbol_aggregates(self, symbol):
-        pass
+        '''
+        Aggregates and returns the stats for the given symbol.
+
+        :param symbol: Symbol in the alphabet
+        :type symbol: string
+        :return result: Stats of the symbol
+        :rtype result: dict
+        '''
+        result = self.get_date_stats(symbol)
+        result['followups'] = self.get_followups_count(symbol)
+        return result
 
     def range_aggregates(self, lower, upper):
         pass
